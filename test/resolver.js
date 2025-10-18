2dac12e04079bb19fa720485fef1c9a651137bd3
metwas <metwas.seup@gmail.com>
2025-10-18 14:39:05 +0100

    resolve [ModuleContext] idea

--- test/resolver.js
+++ test/resolver.js
@@ -2,5 +2,11 @@ const { ModuleRegistry, Test } = require('../dist/main');
 
 const test = ModuleRegistry.resolve(Test);
 
+/** @TODO: resolve from @see ModuleContext  */
+ModuleRegistry.resolveContext({
+       providers: [ TOKEN_PROVIDER_ID ],
+       inject: []
+});
+
 console.log(test);
 // test.request();
