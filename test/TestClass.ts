5390fcfd53c4ffa1aa6c2a58bb541dcc0d6b09d1
metwas <metwas.seup@gmail.com>
2025-12-18 15:26:20 +0000

    Added typescript test files

---
+++ test/TestClass.ts
@@ -0,0 +1,88 @@
+/**
+     MIT License
+
+     @Copyright (c) Metwas
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+
+     The above Copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+*/
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_- Imports  _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+import { SetMetadata, Injectable, Inject } from '../src/main';
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_-          _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+@Injectable('Injected_D')
+class D {
+       public name: string = 'D';
+}
+
+@Injectable()
+class B {
+       public hello() {
+              return 'Hello from B';
+       }
+}
+
+@Injectable()
+export class C {
+       public constructor(public b: B) {}
+
+       public hello() {
+              return 'Hello from C';
+       }
+}
+
+/**
+ * @public
+ */
+export interface Encoder {
+       encode(value: any): string;
+       decode(value: string): any;
+}
+
+/**
+ * Native @see JSON encoder/decoder
+ *
+ * @public
+ */
+@Injectable('JSON_ENCODER')
+export class JsonEncoder implements Encoder {
+       encode(value: any): string {
+              return JSON.stringify(value);
+       }
+
+       decode(value: string): any {
+              return JSON.parse(value);
+       }
+}
+
+/**
+ * @public
+ */
+@Injectable()
+export class Test {
+       /**
+        * Expects @see Encoder interface - in this case the @see JsonEncoder
+        *
+        * @public
+        * @param {Encoder} encoder
+        */
+       public constructor(@Inject('JSON_ENCODER') public encoder: Encoder) {}
+}
